7cf37b82e5006514a1b48b5b4513ec62f3d927d7
Daniil <82492303+daniilfigasystems@users.noreply.github.com>
2025-01-20 09:20:24 +0900

    Added examples

--- examples/example1.c
+++ examples/example1.c
@@ -6,9 +6,9 @@
 char sword[3 * 7] = {
     ' ', 'O', ' ',
     ' ', 'O', ' ',
+    ' ', 'O', ' ',
+    ' ', 'O', ' ',
     'O', 'O', 'O',
-    ' ', 'O', ' ',
-    ' ', 'O', ' ',
     ' ', 'O', ' ',
     ' ', 'O', ' '
 };
@@ -16,20 +16,31 @@ char* name = "test";
 
 int main()
 {
+    /* Initialize ascii library */
     asciifb framebuffer;
     framebuffer.height = 32;
     framebuffer.width = 40;
 
     asciiinit(&framebuffer);
 
+    /* draw circle in framebuffer */
     asciidrawcircle(&framebuffer, 1, 1, 15, 7, '#');
+    /* render it */
     asciirenderfb(&framebuffer);
+    /* clear framebuffer */
     asciiclearfb(&framebuffer);
+    /* draw line in framebuffer */
     asciidrawline(&framebuffer, 1, 1, 9, 4, '*');
+    /* render it */
     asciirenderfb(&framebuffer);
+    /* clear framebuffer*/
     asciiclearfb(&framebuffer);
+    /* copy character array to framebuffer */
     asciicopyatfb(&framebuffer, sword, 5, 5, 3, 7);
+    /* copy text to framebuffer */
     asciicopyatfb(&framebuffer, name, 4, 13, strlen(name), 1);
+    /* render it */
     asciirenderfb(&framebuffer);
+    /* clear it */
     asciifreefb(&framebuffer);
 }
